4f98faf6db9e16a7f85fc89c9f85b1df48a60a7c
Tyler <tylertong519@gmail.com>
2025-01-17 16:20:56 -0800

    Finished Selection Sort

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -25,16 +25,51 @@ namespace BasicSort
             }
             return array;
         }
+
+        public static int FindLeast(int[] array, int startIndex = 0)
+        {
+            int min = int.MaxValue;
+            for (int i = startIndex; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+
+        public static int FindIndex(int number, int[] array, int startIndex = 0)
+        {
+            for (int i = startIndex; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
         public static int[] SelectionSort(int[] array)
         {
-
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min = FindLeast(array, i);
+                int temp = array[i];
+                int temp2 = FindIndex(min, array, i);
+                array[temp2] = temp;
+                array[i] = min;
+                
+            }
+            return array;
         }
         static void Main(string[] args)
         {
-            BubbleSortTest(BubbleSort);
+            SortTest(BubbleSort);
+            SortTest(SelectionSort);
         }
 
-        public static void BubbleSortTest(Func<int[], int[]> sort)
+        public static void SortTest(Func<int[], int[]> sort)
         {
             Random randy = new Random();
             for (int i = 0; i < 100; i++)
